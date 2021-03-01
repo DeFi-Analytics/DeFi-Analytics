47d1e307d559b85e68712415216f48a1ecfb48b5
DanielZirkel <daniel.zirkel@freenet.de>
2021-03-01 21:27:00 +0100

    #2 finished: model adapted, view integrated

---
+++ dashboard/blockchain/coin/coinView.py
@@ -0,0 +1,126 @@
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+
+from plotly.subplots import make_subplots
+
+
+class coinViewClass:
+
+    def getCoinContent(self, data):
+        content = [dbc.Card(dbc.CardBody([dcc.Graph(figure=self.createCoinFigure(data),
+                                                    config={'displayModeBar': False})])),
+                   dbc.Card(dbc.CardBody(self.getCoinExplanation()), style={'margin-top': '1.0rem'})]
+        return content
+
+    @staticmethod
+    def createCoinFigure(data):
+        figDFI = make_subplots(
+            rows=2, cols=1,
+            vertical_spacing=0.15,
+            row_width=[0.5, 0.5],  # from bottom to top
+            specs=[[{}],
+                   [{}]],
+            shared_xaxes=True,
+            subplot_titles=(['Total representation', 'Relative representation']))
+        figDFI.layout.annotations[0].font.color = '#6c757d'  # subplot title font color
+        figDFI.layout.annotations[0].font.size = 20
+        figDFI.layout.annotations[1].font.color = '#6c757d'
+        figDFI.layout.annotations[1].font.size = 20
+
+        # absolut DFI representation
+        trace_mnDFI = dict(type='scatter', name='Masternodes', x=data['mnDFI'].dropna().index, y=data['mnDFI'].dropna(),
+                           mode='lines', line=dict(color='#f44235'), line_width=3, hovertemplate='%{y:,.0f}', legendgroup='MN')
+        figDFI.add_trace(trace_mnDFI, 1, 1)
+
+        trace_fundDFI = dict(type='scatter', name='Community fund', x=data['fundDFI'].dropna().index, y=data['fundDFI'].dropna(),
+                             mode='lines', line=dict(color='#ff9800'), line_width=3, hovertemplate='%{y:,.0f}', legendgroup='CF')
+        figDFI.add_trace(trace_fundDFI, 1, 1)
+
+        trace_foundationDFI = dict(type='scatter', name='Foundation', x=data['foundationDFI'].dropna().index,
+                                   y=data['foundationDFI'].dropna(),mode='lines', line=dict(color='#22b852'), line_width=3,
+                                   hovertemplate='%{y:,.0f}', legendgroup='Found')
+        figDFI.add_trace(trace_foundationDFI, 1, 1)
+
+        trace_otherDFI = dict(type='scatter', name='Other', x=data['otherDFI'].dropna().index,
+                              y=data['otherDFI'].dropna(), mode='lines', line=dict(color='#410eb2'), line_width=3,
+                              hovertemplate='%{y:,.0f}', legendgroup='other')
+        figDFI.add_trace(trace_otherDFI, 1, 1)
+
+        trace_lmDFI = dict(type='scatter', name='Liquidity pool', x=data['lmDFI'].dropna().index, y=data['lmDFI'].dropna(),
+                           mode='lines', line=dict(color='#ff00af'), line_width=3, hovertemplate='%{y:,.0f}', legendgroup='lm')
+        figDFI.add_trace(trace_lmDFI, 1, 1)
+
+
+        # relative DFI representation
+        trace_relMnDFI = dict(type='scatter', name='', x=data['mnDFI'].dropna().index, y=data['mnDFI'].dropna() / data['totalDFI'].dropna() * 100,
+                              mode='lines', line=dict(color='#f44235'), line_width=3, hovertemplate='%{y:,.1f} %', legendgroup='MN', showlegend=False)
+        figDFI.add_trace(trace_relMnDFI, 2, 1)
+
+        trace_relFundDFI = dict(type='scatter', name='', x=data['fundDFI'].dropna().index, y=data['fundDFI'].dropna() / data['totalDFI'].dropna() * 100,
+                                mode='lines', line=dict(color='#ff9800'), line_width=3, hovertemplate='%{y:,.1f} %', legendgroup='CF', showlegend=False)
+        figDFI.add_trace(trace_relFundDFI, 2, 1)
+
+        trace_relFoundationDFI = dict(type='scatter', name='', x=data['totalDFI'].dropna().index, y=data['foundationDFI'].dropna() / data['totalDFI'].dropna() * 100,
+                                      mode='lines', line=dict(color='#22b852'), line_width=3, hovertemplate='%{y:,.1f} %', legendgroup='Found', showlegend=False)
+        figDFI.add_trace(trace_relFoundationDFI, 2, 1)
+
+        trace_relOtherDFI = dict(type='scatter', name='', x=data['otherDFI'].dropna().index, y=data['otherDFI'].dropna() / data['totalDFI'].dropna() * 100,
+                                 mode='lines', line=dict(color='#410eb2'), line_width=3, hovertemplate='%{y:,.1f} %', legendgroup='other', showlegend=False)
+        figDFI.add_trace(trace_relOtherDFI, 2, 1)
+
+        trace_relLMDFI = dict(type='scatter', name='', x=data['totalDFI'].dropna().index, y=data['lmDFI'].dropna() / data['totalDFI'].dropna() * 100,
+                              mode='lines', line=dict(color='#ff00af'), line_width=3, hovertemplate='%{y:,.1f} %', legendgroup='lm', showlegend=False)
+        figDFI.add_trace(trace_relLMDFI, 2, 1)
+
+        figDFI.update_yaxes(title_text='DFI amount', tickformat=",.f", gridcolor='#6c757d', color='#6c757d',
+                            zerolinecolor='#6c757d', row=1, col=1)
+        figDFI.update_yaxes(title_text='Rel. part in %', tickformat=",.1f", gridcolor='#6c757d', color='#6c757d',
+                            zerolinecolor='#6c757d', row=2, col=1)
+        figDFI.update_xaxes(gridcolor='#6c757d', zerolinecolor='#6c757d', row=1, col=1)
+        figDFI.update_xaxes(title_text="Date", gridcolor='#6c757d', color='#6c757d', zerolinecolor='#6c757d', row=2,
+                            col=1)
+
+        figDFI.update_layout(xaxis=dict(
+            rangeselector=dict(
+                buttons=list([dict(count=30, label="30d", step="day", stepmode="backward"),
+                              dict(count=2, label="2m", step="month", stepmode="backward"),
+                              dict(count=6, label="6m", step="month", stepmode="backward"),
+                              dict(count=1, label="YTD", step="year", stepmode="todate"),
+                              dict(count=1, label="1y", step="year", stepmode="backward"),
+                              dict(step="all")])),
+            rangeslider=dict(visible=False),
+            type="date"))
+
+        figDFI.update_layout(height=680,
+                             margin={"t": 40, "l": 130, "b": 20},
+                             hovermode='x unified',
+                             hoverlabel=dict(font_color="#6c757d"),
+                             legend=dict(orientation="h",
+                                         yanchor="bottom",
+                                         y=-0.3,
+                                         xanchor="right",
+                                         x=1),
+                             )
+        figDFI.layout.plot_bgcolor = '#ffffff'  # background plotting area
+        figDFI.layout.paper_bgcolor = 'rgba(0,0,0,0)'  # background around plotting area
+        figDFI.layout.legend.font.color = '#6c757d'  # font color legend
+
+        return figDFI
+
+    def getCoinExplanation(self):
+        coinCardExplanation = [html.H4("Info Coins"),
+                               html.P(['For tracking the amount of DFI and their distribution a snapshot of the DeFiChain richlist is made once a day (in the night).', html.Br(),
+                                       html.A('http://explorer.defichain.io/#/DFI/mainnet/rich-list', href='http://explorer.defichain.io/#/DFI/mainnet/rich-list',
+                                              target='_blank')], style={'text-align': 'justify'}),
+
+                               html.P('The first diagram shows the absolute number of coins and their allocation.', style={'text-align': 'justify'}),
+                               html.P([ 'The second diagram uses the same data base, but is a relative representation. Due to the fact that the DefiChain foundation no '
+                                        'longer has masternodes their relative share is now steadily decreasing'],
+                                      style={'text-align': 'justify'}),
+                               html.P([html.B('Hint:'),
+                                       ' The presented diagrams are interactive. You can zoom in (select range with mouse) and rescale (double-click in diagram) as you like.'
+                                       ' For specific questions it could be helpful to only show a selection of the available data. To exclude entries from the graph click on '
+                                       'the corresponding legend entry.'],
+                                      style={'text-align': 'justify', 'fontSize': '0.7rem', 'color': '#6c757d'})]
+        return coinCardExplanation
--- dashboard/defichainAnalyticsModel.py
+++ dashboard/defichainAnalyticsModel.py
@@ -7,37 +7,52 @@ class defichainAnalyticsModelClass:
         workDir = os.path.abspath(os.getcwd())
         self.dataPath = workDir[:-9] + '/data/'
         self.dailyData = pd.DataFrame()
+        self.hourlyData = pd.DataFrame()
 
         # last update of csv-files
         self.updated_extractedRichlist = None
         self.updated_tradingData = None
         self.updated_blocktime = None
-
+        self.updated_dexHourly = None
 
     #### DAILY DATA #####
     def loadDailyData(self):
+        self.loadHourlyDEXdata()
         self.loadExtractedRichlistData()
         self.loadDailyTradingData()
         self.loadDailyBlocktimeData()
 
     def loadExtractedRichlistData(self):
-        dataPath = self.dataPath + 'extractedDFIdata.csv'
-        fileInfo = pathlib.Path(dataPath)
+        filePath = self.dataPath + 'extractedDFIdata.csv'
+        fileInfo = pathlib.Path(filePath)
         if fileInfo.stat() != self.updated_extractedRichlist:
-            extractedRichlist = pd.read_csv(self.dataPath+'extractedDFIdata.csv',index_col=0)
+            extractedRichlist = pd.read_csv(filePath, index_col=0)
 
             ind2Delete = self.dailyData.columns.intersection(extractedRichlist.columns)                                 # check if columns exist
-            self.dailyData.drop(columns=ind2Delete, inplace=True)                                                       # delete exisiting columns to add new ones
-            self.dailyData = self.dailyData.merge(extractedRichlist, how='outer',left_index=True,right_index=True)      # add new columns to daily table
+            self.dailyData.drop(columns=ind2Delete, inplace=True)                                                       # delete existing columns to add new ones
+            self.dailyData = self.dailyData.merge(extractedRichlist, how='outer', left_index=True, right_index=True)      # add new columns to daily table
 
-            self.dailyData['nbOverall'] =self.dailyData['nbMnId'] +self.dailyData['nbOtherId'] +1
+            # extracting DFI in Liquidity-Mining
+            lmCoins = pd.DataFrame(index=self.dailyData.index)
+            lmCoins['BTC_pool'] = self.hourlyData[self.hourlyData.symbol == 'BTC-DFI'].groupby('date')['reserveB'].first()
+            lmCoins['ETH_pool'] = self.hourlyData[self.hourlyData.symbol == 'ETH-DFI'].groupby('date')['reserveB'].first()
+            lmCoins['USDT_pool'] = self.hourlyData[self.hourlyData.symbol == 'USDT-DFI'].groupby('date')['reserveB'].first()
+            lmCoins['DOGE_pool'] = self.hourlyData[self.hourlyData.symbol == 'DOGE-DFI'].groupby('date')['reserveB'].first()
+            lmCoins['LTC_pool'] = self.hourlyData[self.hourlyData.symbol == 'LTC-DFI'].groupby('date')['reserveB'].first()
+            lmCoins['overall'] = lmCoins['BTC_pool'] + lmCoins['ETH_pool'] + lmCoins['USDT_pool'] + lmCoins['DOGE_pool'].fillna(0) + lmCoins['LTC_pool'].fillna(0)
+            self.dailyData['lmDFI'] = lmCoins['overall']
+
+            # total amount of addresses and DFI
+            self.dailyData['nbOverall'] = self.dailyData['nbMnId'] + self.dailyData['nbOtherId']
+            self.dailyData['totalDFI'] = self.dailyData['fundDFI'] + self.dailyData['mnDFI'] + self.dailyData['otherDFI'] + \
+                                       self.dailyData['foundationDFI'].fillna(0) + self.dailyData['lmDFI'].fillna(0)
 
             self.updated_extractedRichlist = fileInfo.stat()
             print('>>>> Richlist data loaded from csv-file <<<<')
 
     def loadDailyTradingData(self):
-        dataPath = self.dataPath + 'dailyTradingResultsDEX.csv'
-        fileInfo = pathlib.Path(dataPath)
+        filePath = self.dataPath + 'dailyTradingResultsDEX.csv'
+        fileInfo = pathlib.Path(filePath)
         if fileInfo.stat() != self.updated_tradingData:
             dailyTradingResults = pd.read_csv(self.dataPath+'dailyTradingResultsDEX.csv',index_col=0)
 
@@ -49,10 +64,10 @@ class defichainAnalyticsModelClass:
             print('>>>> Trading data loaded from csv-file <<<<')
 
     def loadDailyBlocktimeData(self):
-        dataPath = self.dataPath + 'BlockListStatistics.csv'
-        fileInfo = pathlib.Path(dataPath)
+        filePath = self.dataPath + 'BlockListStatistics.csv'
+        fileInfo = pathlib.Path(filePath)
         if fileInfo.stat() != self.updated_blocktime:
-            dailyBlocktimeData = pd.read_csv(dataPath, index_col=0)
+            dailyBlocktimeData = pd.read_csv(filePath, index_col=0)
             dailyBlocktimeData['tps'] = dailyBlocktimeData['txCount'] / (24 * 60 * 60)
 
             ind2Delete = self.dailyData.columns.intersection(dailyBlocktimeData.columns)                                # check if columns exist
@@ -62,7 +77,15 @@ class defichainAnalyticsModelClass:
             self.updated_blocktime = fileInfo.stat()
             print('>>>> Blocktime data loaded from csv-file <<<<')
 
+    def loadHourlyDEXdata(self):
+        filePath = self.dataPath + 'LMPoolData.csv'
+        fileInfo = pathlib.Path(filePath)
+        if fileInfo.stat() != self.updated_dexHourly:
+            self.hourlyData = pd.read_csv(filePath, index_col=0)
+            self.hourlyData['date'] = pd.to_datetime(self.hourlyData.Time).dt.strftime('%Y-%m-%d')
 
+            self.updated_dexHourly = fileInfo.stat()
+            print('>>>> Hourly DEX data loaded from csv-file <<<<')
 
     #### MINUTELY DATA ####
     def loadShortTermDEXPrice(self):
