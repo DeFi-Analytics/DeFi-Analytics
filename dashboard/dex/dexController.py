8e362a24fe84919da584cc5689b5524d0774d510
DanielZirkel <daniel.zirkel@freenet.de>
2021-03-15 20:18:28 +0100

    #16 add callback to show info

--- dashboard/dex/dexController.py
+++ dashboard/dex/dexController.py
@@ -1,9 +1,5 @@
-import dash_html_components as html
-# from .addresses.addressesView import addressesViewClass
-# from .addresses.addressesCallbacks import addressesCallbacksClass
-
 from .volume.volumeView import volumeViewClass
-
+from .volume.volumeCallbacks import volumeCallbacksClass
 
 class dexControllerClass:
     def __init__(self, app, defichainAnalyticsModel):
@@ -12,7 +8,7 @@ class dexControllerClass:
 
         # initialize fees classes
         self.volumeView = volumeViewClass()
-        # self.addressesCallbacks = addressesCallbacksClass()       # create callbacks on top level
+        self.volumeCallbacks = volumeCallbacksClass(app)       # create callbacks on top level
 
 
 
