215abb44d8b8d7f4be21e4415a63e9702ce91725
DanielZirkel <daniel.zirkel@freenet.de>
2021-04-06 23:00:29 +0200

    #48 first script version

--- script4Task/getTwitterFollower.py
+++ script4Task/getTwitterFollower.py
@@ -4,7 +4,8 @@ import pandas as pd
 from datetime import datetime
 
 scriptPath = __file__
-path = scriptPath[:-36] + '/data/'
+path = scriptPath[:-34] + '/data/'
+filepath = path +'TwitterData_follower.csv'
 
 auth = tw.OAuthHandler('ffPBjkGwiuDJQM3KOdYy0cz5H', 'LaqDMQhyJ4nT9bOat96oMpiUJoJfkpr0BZUVlzof9UaQmQXAVQ')
 auth.set_access_token('1325833977836556288-4eBpurdKwwloCm9xmUYLT41NhagHcz', 'UWQgE20rtf9d4xVZYnhllkHGppdcjsAeKaoqtGu7OfNWf')
@@ -14,10 +15,15 @@ dateTimeObj = datetime.now()
 strTimestamp = dateTimeObj.strftime("%Y-%m-%d %H:%M")
 
 ids = []
-for page in tw.Cursor(api.followers_ids, screen_name="DanielZirkel").pages():
+for page in tw.Cursor(api.followers_ids, screen_name="defichain").pages():
     ids.extend(page)
 
-dfTwitterFollower = pd.DataFrame()
+dfCurrentTwitterFollower = pd.DataFrame(index=ids)
+dfCurrentTwitterFollower[strTimestamp] = 1
 
+# dfTwitterFollower = pd.read_csv(filepath,index_col=0)
 
-dfTwitterFollower[]
+# add twitter follower IDs to exisiting data
+#dfTwitterFollower[strTimestamp] = ids
+
+dfCurrentTwitterFollower.to_csv(filepath)
