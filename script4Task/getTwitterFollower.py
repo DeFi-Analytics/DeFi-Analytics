817e07c33ddf31cb3fdc92ba171f2b39a0b5ff42
DanielZirkel <daniel.zirkel@freenet.de>
2021-04-20 22:48:07 +0200

    #48 adapt script for data acquisition

--- script4Task/getTwitterFollower.py
+++ script4Task/getTwitterFollower.py
@@ -1,4 +1,3 @@
-import os
 import tweepy as tw
 import pandas as pd
 from datetime import datetime
@@ -6,24 +5,42 @@ from datetime import datetime
 scriptPath = __file__
 path = scriptPath[:-34] + '/data/'
 filepath = path +'TwitterData_follower.csv'
+filepathLastList = path +'TwitterData_lastFollowerList.csv'
 
+# create API for Twitter access
 auth = tw.OAuthHandler('ffPBjkGwiuDJQM3KOdYy0cz5H', 'LaqDMQhyJ4nT9bOat96oMpiUJoJfkpr0BZUVlzof9UaQmQXAVQ')
 auth.set_access_token('1325833977836556288-4eBpurdKwwloCm9xmUYLT41NhagHcz', 'UWQgE20rtf9d4xVZYnhllkHGppdcjsAeKaoqtGu7OfNWf')
 api = tw.API(auth, wait_on_rate_limit=True)
 
+# get current date
 dateTimeObj = datetime.now()
-strTimestamp = dateTimeObj.strftime("%Y-%m-%d %H:%M")
+strTimestamp = dateTimeObj.strftime("%Y-%m-%d")
 
+# create list of defichain followers
 ids = []
 for page in tw.Cursor(api.followers_ids, screen_name="defichain").pages():
     ids.extend(page)
+dfCurrentTwitterFollower = pd.DataFrame()
+dfCurrentTwitterFollower[strTimestamp] = ids
 
-dfCurrentTwitterFollower = pd.DataFrame(index=ids)
-dfCurrentTwitterFollower[strTimestamp] = 1
+# get absolute number of followers
+nbFollower = dfCurrentTwitterFollower.shape[0]
+
+# compare current follower list with old list
+dfOldTwitterFollower = pd.read_csv(filepathLastList, index_col=0)
+nbFollowed = (~dfCurrentTwitterFollower.iloc[:, 0].isin(dfOldTwitterFollower.iloc[:, 0])).sum()
+nbUnfollowed = (~dfOldTwitterFollower.iloc[:, 0].isin(dfCurrentTwitterFollower.iloc[:, 0])).sum()
+
+# extract data from old and new follower list; add to exisiting database
+#dfTwitterFollowerData = pd.DataFrame(columns=['Date', 'Follower', 'followedToday', 'unfollowedToday'])         # if a need database is needed
+dfTwitterFollowerData = pd.read_csv(filepath, index_col=0)
+newDataSeries = pd.Series([strTimestamp, nbFollower, nbFollowed, nbUnfollowed], index=dfTwitterFollowerData.columns, name=strTimestamp)
+dfTwitterFollowerData = dfTwitterFollowerData.append(newDataSeries, ignore_index=True)
+
 
 # dfTwitterFollower = pd.read_csv(filepath,index_col=0)
 
-# add twitter follower IDs to exisiting data
+# saving data
 #dfTwitterFollower[strTimestamp] = ids
-
-dfCurrentTwitterFollower.to_csv(filepath)
+dfTwitterFollowerData.to_csv(filepath)
+dfCurrentTwitterFollower.to_csv(filepathLastList)
