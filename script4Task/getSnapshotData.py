67287f1025188fb38f5793785926e8cec752f63f
DanielZirkel <daniel.zirkel@freenet.de>
2021-06-19 16:09:56 +0200

    #127 adapt script getSnapshotData

--- script4Task/getSnapshotData.py
+++ script4Task/getSnapshotData.py
@@ -77,7 +77,7 @@ while True:
         siteContent = requests.get(linkBurninfo)
         if siteContent.status_code==200:
             tempData = pd.read_json(siteContent.text).transpose()
-            burnedDFIFees = tempData.iloc[2, 0]
+            burnedDFIFees = tempData.loc['feeburn', 0]
         else:
             burnedDFIFees = np.NaN
 
