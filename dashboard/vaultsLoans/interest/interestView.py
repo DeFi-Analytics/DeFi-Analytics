39ab9b29307fc7e8a3cd979002cc342a13334005
DanielZirkel <daniel.zirkel@freenet.de>
2022-01-09 20:35:03 +0100

    #181 change number representation hovertemplate

--- dashboard/vaultsLoans/interest/interestView.py
+++ dashboard/vaultsLoans/interest/interestView.py
@@ -44,7 +44,7 @@ class interestViewClass:
         date14DaysBack = lastValidDate - dateutil.relativedelta.relativedelta(days=14)
 
         trace_nbDToken = dict(type='scatter', name='DEX price in dUSD', x=(data['sumInterest']*price).dropna().index, y=(data['sumInterest']*price).dropna(),
-                                 mode='lines', line=dict(color='#ff00af'), line_width=3, hovertemplate='%{y:.f}')
+                                 mode='lines', line=dict(color='#ff00af'), line_width=3, hovertemplate='%{y:,.f}')
 
         figNbDToken.add_trace(trace_nbDToken, 1, 1)
 
