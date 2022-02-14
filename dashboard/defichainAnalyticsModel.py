9d0867975db53850382f9e9ff6500783db30178d
DanielZirkel <daniel.zirkel@freenet.de>
2022-02-14 21:27:57 +0100

    #200 fixed

--- dashboard/defichainAnalyticsModel.py
+++ dashboard/defichainAnalyticsModel.py
@@ -61,6 +61,7 @@ class defichainAnalyticsModelClass:
 
     #### DAILY DATA #####
     def loadDailyData(self):
+        self.loadCoinPriceList()
         self.loadHourlyDEXdata()
         self.loadDEXVolume()
         self.loadDailyTradingData()
@@ -78,7 +79,7 @@ class defichainAnalyticsModelClass:
         self.loadDFIsignalDatabase()
         self.loadDobbyDatabase()
         self.loadEmissionRateData()
-        self.loadCoinPriceList()
+
 
 
 
@@ -217,16 +218,17 @@ class defichainAnalyticsModelClass:
         fileInfo = pathlib.Path(filePath)
         if fileInfo.stat() != self.updated_tradingData:
             dailyTradingResults = pd.read_csv(self.dataPath+'dailyTradingResultsDEX.csv',index_col=0)
+            dailyTradingResults.drop(['DFIPriceUSD', 'BTCPriceUSD', 'ETHPriceUSD', 'USDTPriceUSD', 'DOGEPriceUSD', 'LTCPriceUSD', 'BCHPriceUSD', 'USDCPriceUSD'], axis=1, inplace=True)     # remove information and use coinpricelist file
 
             ind2Delete = self.dailyData.columns.intersection(dailyTradingResults.columns)                               # check if columns exist
             self.dailyData.drop(columns=ind2Delete, inplace=True)                                                       # delete existing columns to add new ones
             self.dailyData = self.dailyData.merge(dailyTradingResults, how='outer', left_index=True, right_index=True)    # add new columns to daily table
 
             # calc market cap data in USD and BTC (same as in loadExtractedRichlistData to get updated price information
-            if 'circDFI' in self.dailyData.columns:
-                print('>>>>>>>> Update market cap in loadDailyTradingData...  <<<<<<<<')
-                self.dailyData['marketCapUSD'] = self.dailyData['circDFI']*self.dailyData['DFIPriceUSD']
-                self.dailyData['marketCapBTC'] = self.dailyData['marketCapUSD'] / self.dailyData['BTCPriceUSD']
+            # if 'circDFI' in self.dailyData.columns:
+            #     print('>>>>>>>> Update market cap in loadDailyTradingData...  <<<<<<<<')
+            #     self.dailyData['marketCapUSD'] = self.dailyData['circDFI']*self.dailyData['DFIPriceUSD']
+            #     self.dailyData['marketCapBTC'] = self.dailyData['marketCapUSD'] / self.dailyData['BTCPriceUSD']
 
             self.updated_tradingData = fileInfo.stat()
             print('>>>> Trading data loaded from csv-file <<<< ==== Columns: '+str(len(self.dailyData.columns))+'  Rows: '+str(len(self.dailyData.index)))
