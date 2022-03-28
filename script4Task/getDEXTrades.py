89fc9d716271e79af91b95aa0b7c60f5dfe83691
DanielZirkel <daniel.zirkel@freenet.de>
2022-03-28 21:59:06 +0200

    start rework getDEXTrades

--- script4Task/getDEXTrades.py
+++ script4Task/getDEXTrades.py
@@ -1,5 +1,9 @@
 import requests
 import pandas as pd
+import numpy as np
+import json
+from datetime import datetime
+import time
 
 # generate filepath relative to script location
 scriptPath = __file__
@@ -10,7 +14,74 @@ filepathTradingResults = path + 'dailyTradingResultsDEX.csv'
 
 
 availablePools = {'ETH':4,'BTC':5,'USDT':6,'DOGE':8,'LTC':10,'BCH':12,'USDC':14}
+availablePools = {'BTC':5,'USDT':6,'DOGE':8,'LTC':10,'BCH':12,'USDC':14}
 dfTradeResult = pd.DataFrame()
+
+for key in availablePools:
+    print('Getting swaps of pool: ' + key)
+    dfSwaplist = pd.DataFrame()
+
+    newData = True
+    nextPage = ''
+    while newData:
+        linkSwaps = 'https://ocean.defichain.com/v0/mainnet/poolpairs/' + str(availablePools.get(key)) + '/swaps/verbose'+nextPage
+        siteContent = requests.get(linkSwaps)
+        while siteContent.status_code == 500:
+            print('Retry API Call')
+            time.sleep(2)
+            siteContent = requests.get(linkSwaps)
+
+
+
+        tempData = json.loads(siteContent.text)
+
+        # check if another page must be requested
+        if 'page' in tempData:
+            nextPage = '?next=' + tempData['page']['next']
+        else:
+            newData = False
+
+        tempDataframe = pd.DataFrame(tempData['data'])
+        tempDataframe['Time'] = [datetime.utcfromtimestamp(element.get('medianTime')).strftime('%Y-%m-%d %H:%M:%S')  for element in tempDataframe['block'] ]
+        tempDataframe['blockNb'] = [element.get('medianTime') for element in tempDataframe['block'] ]
+        tempDataframe['fromSymbol'] = [element.get('symbol') for element in tempDataframe['from']]
+        tempDataframe['toAmount'] = [element.get('amount') if element is not np.nan else np.nan for element in tempDataframe['to']]
+        tempDataframe['toSymbol'] = [element.get('symbol') if element is not np.nan else np.nan for element in tempDataframe['to']]
+        tempDataframe.drop(['id', 'sort', 'fromTokenId', 'block', 'from', 'to'], axis=1, inplace=True)
+        tempDataframe.set_index('txid', inplace=True)
+
+        listNewSwaps = tempDataframe.index.difference(dfSwaplist.index)
+        listOldSwaps = tempDataframe.index.intersection(dfSwaplist.index)
+        tempDataframe.loc[listNewSwaps]
+        newData = listOldSwaps.empty
+        print(nextPage + ' ' + str(newData))
+        dfSwaplist = dfSwaplist.append(tempDataframe.loc[listNewSwaps], verify_integrity=True)
+
+    #     # evaluate the data inside the vaults
+    #     for item in tempData['data']:
+    #         vaultData['nbVaults'] += 1
+    #         if item['state'] == 'ACTIVE':
+    #             vaultData['nbLoans'] += len(item['loanAmounts'])
+    #             vaultData[item['loanScheme']['id']] += 1
+    #             vaultData['sumInterest'] += float(item['interestValue'])
+    #             for coinsCollateral in item['collateralAmounts']:
+    #                 vaultData['sum'+coinsCollateral['symbol']] += float(coinsCollateral['amount'])
+    #             for coinsMinted in item['loanAmounts']:
+    #                 vaultData['sumLoan'+coinsMinted['symbol']] += float(coinsMinted['amount'])
+    #
+    #         elif item['state'] == 'IN_LIQUIDATION':
+    #             for batches in item['batches']:
+    #                 vaultData['sumLoanLiquidation'+batches['loan']['symbol']] += float(batches['loan']['amount'])
+    #             vaultData['nbLiquidation'] += 1
+    #
+    #     vaultData.name = pd.Timestamp.now()
+    #
+    # # save file
+    # # dfOldVaultData = pd.DataFrame()
+    # dfOldVaultData = pd.read_csv(filepath, index_col=0)
+    # dfVaultData = dfOldVaultData.append(vaultData, sort=False)
+    # dfVaultData.to_csv(filepath)
+
 
 # get all swaps over all pools
 for key in availablePools: 
@@ -32,9 +103,9 @@ for key in availablePools:
 
 
 # add usd price information for all coins
-dfCoinPrices = pd.read_csv(filepathCoinPrices,index_col=0)
-dfTradeResult = dfTradeResult.iloc[:-1].merge(dfCoinPrices[['DFIPriceUSD','BTCPriceUSD','ETHPriceUSD','USDTPriceUSD','DOGEPriceUSD','LTCPriceUSD','BCHPriceUSD','USDCPriceUSD']],
-                                    how='outer',left_index=True, right_index=True)
+# dfCoinPrices = pd.read_csv(filepathCoinPrices,index_col=0)
+# dfTradeResult = dfTradeResult.iloc[:-1].merge(dfCoinPrices[['DFIPriceUSD','BTCPriceUSD','ETHPriceUSD','USDTPriceUSD','DOGEPriceUSD','LTCPriceUSD','BCHPriceUSD','USDCPriceUSD']],
+#                                     how='outer',left_index=True, right_index=True)
 
 # calculate trading volume in USD for each base coin and sum of pool
 print('Calculating USD trading volume')
