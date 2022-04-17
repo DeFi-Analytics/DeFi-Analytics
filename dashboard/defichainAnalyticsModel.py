9ffb2c28e03293a856891e02f5f1fd8d032a955a
DanielZirkel <daniel.zirkel@freenet.de>
2022-04-17 23:20:02 +0200

    #226, #229 change data loading procedures

--- dashboard/defichainAnalyticsModel.py
+++ dashboard/defichainAnalyticsModel.py
@@ -5,7 +5,10 @@ import re
 import base64
 
 import pandas as pd
+import numpy as np
 from ast import literal_eval
+
+import time
 
 from datetime import datetime, timedelta
 
@@ -52,6 +55,7 @@ class defichainAnalyticsModelClass:
         self.updated_vaults = None
         self.update_emissionRate=None
         self.update_coinPriceList = None
+        self.updated_DFIPFutures = None
 
 
         # background image for figures
@@ -478,6 +482,7 @@ class defichainAnalyticsModelClass:
         self.loadHourlyDEXTrades()
         self.loadDFXdata()
         self.loadVaultData()
+        self.loadDFIPFuturesData()
 
 
     def loadHourlyDEXdata(self):
@@ -499,10 +504,9 @@ class defichainAnalyticsModelClass:
             hourlyDEXData['timeRounded'] = pd.to_datetime(hourlyDEXData.Time).dt.floor('H')
             hourlyDEXData.set_index(['timeRounded'], inplace=True)
             hourlyDEXData['reserveA_DFI'] = hourlyDEXData['reserveA'] / hourlyDEXData['DFIPrices']
-
             for poolSymbol in hourlyDEXData.symbol.dropna().unique():
                 df2Add = hourlyDEXData[hourlyDEXData.symbol == poolSymbol]
-                df2Add = df2Add.drop(columns=['Time', 'symbol'])
+                df2Add = df2Add.drop(columns=['Time', 'symbol', 'idTokenA', 'idTokenB'])
 
                 # calculate locked DFI and corresponding values
                 df2Add = df2Add.assign(lockedDFI=df2Add['reserveB'] + df2Add['reserveA_DFI'])
@@ -536,18 +540,15 @@ class defichainAnalyticsModelClass:
             volumeData['timeRounded'] = pd.to_datetime(volumeData.Time).dt.floor('H')
             volumeData.set_index(['timeRounded'], inplace=True)
 
+            df2Add = pd.DataFrame()
             for poolSymbol in volumeData['base_name'].unique():
-                df2Add = volumeData[volumeData['base_name']==poolSymbol][['base_volume', 'quote_volume']]
-                df2Add['VolTotal'] = df2Add[['base_volume', 'quote_volume']].sum(axis=1)
-                # add prefix to column names for pool identification
-                colNamesOrig = df2Add.columns.astype(str)
-                colNamesNew = poolSymbol + '_' + colNamesOrig
-                df2Add = df2Add.rename(columns=dict(zip(colNamesOrig, colNamesNew)))
+                df2Add[poolSymbol + '_'+'VolTotal'] = volumeData[volumeData['base_name']==poolSymbol][['base_volume', 'quote_volume']].sum(axis=1)
 
-                # delete existing information and add new one
-                ind2Delete = self.hourlyData.columns.intersection(colNamesNew)                                          # check if columns exist
-                self.hourlyData.drop(columns=ind2Delete, inplace=True)                                                          # delete existing columns to add new ones
-                self.hourlyData = self.hourlyData.merge(df2Add, how='outer', left_index=True, right_index=True)           # add new columns to daily table
+            # delete existing information and add new one
+            colNames = df2Add.columns
+            ind2Delete = self.hourlyData.columns.intersection(colNames)                                          # check if columns exist
+            self.hourlyData.drop(columns=ind2Delete, inplace=True)                                                          # delete existing columns to add new ones
+            self.hourlyData = self.hourlyData.merge(df2Add, how='outer', left_index=True, right_index=True)           # add new columns to daily table
 
             # calculate total volume after merge of data
             self.hourlyData['VolTotal'] = self.hourlyData['BTC_VolTotal']*0   # only use rows with data; BTC was the first pool and have to most data (beside ETH, USDT)
@@ -646,24 +647,23 @@ class defichainAnalyticsModelClass:
             vaultsData.set_index(['timeRounded'], inplace=True)
 
             tempDataFormat = pd.DataFrame()
-            tempDataFormat['dexfeetokensRaw'] = vaultsData[~vaultsData.dexfeetokens.isna()].dexfeetokens.apply(lambda x: literal_eval(str(x)))
-            tempDataFormat['dfipaybacktokens'] = vaultsData[~vaultsData.dfipaybacktokens.isna()].dfipaybacktokens.apply(lambda x: literal_eval(str(x)))
+            # tempDataFormat['dexfeetokensRaw'] = vaultsData[~vaultsData.dexfeetokens.isna()].dexfeetokens.apply(lambda x: literal_eval(str(x)))
+            # tempDataFormat['dfipaybacktokens'] = vaultsData[~vaultsData.dfipaybacktokens.isna()].dfipaybacktokens.apply(lambda x: literal_eval(str(x)))
 
-            for index, value in tempDataFormat['dexfeetokensRaw'].items():
-                tempDF = pd.Series(value)
-                colNameFeeTokens = tempDF.apply((lambda x: 'burned'+x[x.find('@')+1:]+'DEX'))
-                tempDataFormat.loc[index, colNameFeeTokens] = value
+            for index, value in vaultsData[~vaultsData.dexfeetokens.isna()].dexfeetokens.items():
+                colNameFeeTokens = ['burned'+x[x.find('@')+1:]+'DEX' for x in literal_eval(value)]
+                vaultsData.loc[index,colNameFeeTokens] = [float(x[:x.find('@')]) for x in literal_eval(value)]
 
-            tempDataFormat[['DUSDpaidDFI']] = pd.DataFrame(tempDataFormat['dfipaybacktokens'].tolist(), index=tempDataFormat.index)
-            vaultsData['burnedDUSDDEX'] = tempDataFormat['burnedDUSDDEX'].apply(lambda x: float(x[:-5]))
-            vaultsData['DUSDpaidDFI'] = tempDataFormat['DUSDpaidDFI'].apply(lambda x: float(x[:-5]))
+            burnedValues = [item for item in vaultsData.columns if "burned" in item if "DEX" in item]
+
+            vaultsData['DUSDpaidDFI'] = vaultsData['dfipaybacktokens'].apply(lambda x: float(str(x)[2:str(x).find('@')]) if isinstance(x,str) else np.nan)
 
             sumValues = [item for item in vaultsData.columns if "sum" in item]
             liveTicker = [item[7:]+'-USD' for item in vaultsData.columns if (("sumLoan") in item) & ~(('sumLoanLiquidation') in item)]
             liveTicker += ['DFI-USD']
 
-            columns2update = sumValues + liveTicker + ['nbLiquidation', 'nbLoans', 'nbVaults', 'burnedAuction', 'burnedPayback', 'burnedDFIPayback', 'MIN150', 'MIN175',
-                                                       'MIN200', 'MIN350', 'MIN500', 'MIN1000', 'burnedDUSDDEX', 'DUSDpaidDFI']
+            columns2update = burnedValues + sumValues + liveTicker + ['nbLiquidation', 'nbLoans', 'nbVaults', 'burnedAuction', 'burnedPayback', 'burnedDFIPayback', 'MIN150', 'MIN175',
+                                                       'MIN200', 'MIN350', 'MIN500', 'MIN1000', 'DUSDpaidDFI']
             columns2update.remove('DUSD-USD')
 
             # delete existing information and add new one
@@ -673,6 +673,24 @@ class defichainAnalyticsModelClass:
 
             self.updated_vaults = fileInfo.stat()
             print('>>>> Vaults data loaded from csv-file <<<< ==== Columns: '+str(len(self.hourlyData.columns))+'  Rows: '+str(len(self.hourlyData.index)))
+
+    def loadDFIPFuturesData(self):
+        print('>>>> Start update DFIP futures data ... <<<<')
+        filePath = self.dataPath + 'DFIPFuturesData.csv'
+        fileInfo = pathlib.Path(filePath)
+        if fileInfo.stat() != self.updated_DFIPFutures:
+            DFIPFuturesData = pd.read_csv(filePath, index_col=0)
+            DFIPFuturesData['timeRounded'] = pd.to_datetime(DFIPFuturesData.index).floor('H')
+            DFIPFuturesData.set_index(['timeRounded'], inplace=True)
+
+            # delete existing information and add new one
+            ind2Delete = self.hourlyData.columns.intersection(DFIPFuturesData.columns)                                                               # check if columns exist
+            self.hourlyData.drop(columns=ind2Delete, inplace=True)                                                                          # delete existing columns to add new ones
+            self.hourlyData = self.hourlyData.merge(DFIPFuturesData, how='outer', left_index=True, right_index=True)            # add new columns to daily table
+
+            self.updated_DFIPFutures = fileInfo.stat()
+            print('>>>> DFIP futures data loaded from csv-file <<<< ==== Columns: '+str(len(self.hourlyData.columns))+'  Rows: '+str(len(self.hourlyData.index)))
+
 
     #### MINUTELY DATA ####
     def loadMinutelyData(self):
