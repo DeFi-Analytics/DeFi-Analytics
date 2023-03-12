45fd6ea0d5afc39eab8af4204d09d00194bd1917
DanielZirkel <daniel.zirkel@freenet.de>
2023-03-12 11:01:35 +0100

    #280 scripted adapted

--- script4Task/getDFXdata.py
+++ script4Task/getDFXdata.py
@@ -13,27 +13,36 @@ filepathSummary = path + 'dfxData.csv'
 filepathComplete = path + 'dfxAllOrdersData.csv'
 
 # API request for complete transaction list of DFX
-link='https://api.dfx.swiss/v1/statistic/transactions'
+timeStampData = pd.Timestamp.now() - pd.Timedelta(days=2)
+fromDateAPI = timeStampData.strftime("%Y-%m-%d")
+link='https://api.dfx.swiss/v1/statistic/transactions?dateFrom='+fromDateAPI
 siteContent = requests.get(link)
 tempData= json.loads(siteContent.text)
 
 # load existing list of orders
-dfDFXorders = pd.DataFrame()
+dfDFXorders = pd.read_csv(filepathComplete, index_col=0)
+dfDFXorders.index = pd.to_datetime(dfDFXorders.index)
 
+
+# get all new buy orders and add them to existing dataframe
 tempDataframe = pd.DataFrame(tempData['buy'])
 tempDataframe['order'] = 'buy'
-dfDFXorders = dfDFXorders.append(tempDataframe)
+tempDataframe['date'] = pd.to_datetime(tempDataframe['date'])
+tempDataframe.set_index('date', inplace=True)
+index2Add = ~tempDataframe.index.isin(dfDFXorders.index)
+dfDFXorders = dfDFXorders.append(tempDataframe[index2Add])
 
+
+# get all new sell orders and add them to existing dataframe
 tempDataframe = pd.DataFrame(tempData['sell'])
 tempDataframe['order'] = 'sell'
-dfDFXorders = dfDFXorders.append(tempDataframe)
+tempDataframe['date'] = pd.to_datetime(tempDataframe['date'])
+tempDataframe.set_index('date', inplace=True)
+index2Add = ~tempDataframe.index.isin(dfDFXorders.index)
+dfDFXorders = dfDFXorders.append(tempDataframe[index2Add])
 
-dfDFXorders.drop_duplicates(inplace=True)
-dfDFXorders['date'] = pd.to_datetime(dfDFXorders['date'])
 dfDFXorders.sort_values(by='date', inplace=True)
-dfDFXorders.set_index('date', inplace=True)
-
-# dfDFXorders.to_csv(filepathComplete)
+dfDFXorders.to_csv(filepathComplete)
 
 
 # get hourly data
--- script4Task/getHourlyData.py
+++ script4Task/getHourlyData.py
@@ -410,6 +410,55 @@ def getLOCKData():
     # writing file
     dfLOCKData.to_csv(filepathSummary)
 
+def getDFXData():
+    filepathSummary = path + 'dfxData.csv'
+    filepathComplete = path + 'dfxAllOrdersData.csv'
+
+    print('   start DFX data acquisition')
+
+    # API request for complete transaction list of DFX
+    timeStampData = pd.Timestamp.now() - pd.Timedelta(days=2)
+    fromDateAPI = timeStampData.strftime("%Y-%m-%d")
+    link='https://api.dfx.swiss/v1/statistic/transactions?dateFrom='+fromDateAPI
+    siteContent = requests.get(link)
+    tempData= json.loads(siteContent.text)
+
+    # load existing list of orders
+    dfDFXorders = pd.read_csv(filepathComplete, index_col=0)
+    dfDFXorders.index = pd.to_datetime(dfDFXorders.index)
+
+
+    # get all new buy orders and add them to existing dataframe
+    tempDataframe = pd.DataFrame(tempData['buy'])
+    tempDataframe['order'] = 'buy'
+    tempDataframe['date'] = pd.to_datetime(tempDataframe['date'])
+    tempDataframe.set_index('date', inplace=True)
+    index2Add = ~tempDataframe.index.isin(dfDFXorders.index)
+    dfDFXorders = dfDFXorders.append(tempDataframe[index2Add])
+
+
+    # get all new sell orders and add them to existing dataframe
+    tempDataframe = pd.DataFrame(tempData['sell'])
+    tempDataframe['order'] = 'sell'
+    tempDataframe['date'] = pd.to_datetime(tempDataframe['date'])
+    tempDataframe.set_index('date', inplace=True)
+    index2Add = ~tempDataframe.index.isin(dfDFXorders.index)
+    dfDFXorders = dfDFXorders.append(tempDataframe[index2Add])
+
+    dfDFXorders.sort_values(by='date', inplace=True)
+    dfDFXorders.to_csv(filepathComplete)
+
+
+    # get hourly data
+    dfDFXData = pd.DataFrame(columns=['dfxBuyVolume', 'dfxSellVolume'])
+    dfDFXData['dfxBuyVolume'] = dfDFXorders[dfDFXorders.order == 'buy'].fiatAmount.groupby(pd.Grouper(freq='H')).sum()
+    dfDFXData['dfxBuyVolume'] = dfDFXData['dfxBuyVolume'].fillna(0).cumsum()
+    dfDFXData['dfxSellVolume'] = dfDFXorders[dfDFXorders.order == 'sell'].fiatAmount.groupby(pd.Grouper(freq='H')).sum()
+    dfDFXData['dfxSellVolume'] = dfDFXData['dfxSellVolume'].fillna(0).cumsum()
+
+    dfDFXData.index = dfDFXData.index.strftime('%Y-%m-%d %H:%M')
+    # writing file
+    dfDFXData.to_csv(filepathSummary)
 
 
 timeStampData = pd.Timestamp.now()
@@ -449,4 +498,9 @@ try:
 except:
     print('### Error in LOCK data acquisition')
 
-
+# DFX data
+try:
+    getDFXData()
+    print('DFX data saved')
+except:
+    print('### Error in DFX data acquisition')
